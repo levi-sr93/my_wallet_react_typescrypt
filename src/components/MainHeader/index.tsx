155295bd7bbb65ff0c968999d1fe21f7d19d9584
Levi Santos <levi-sr@outlook.com>
2020-09-30 23:03:12 -0300

    Desenvolvendo  o componente MainHeader

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+import { Container, Profile, Welcome, UserName } from './styles';
+
+import emojis from '../../utils/emojis';
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * emojis.length);
+    // console.log(index);
+    return emojis[index];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader Component</h1>
+      <h1>toogle</h1>
+      <Profile>
+        <Welcome>Hello {emoji}</Welcome>
+        <UserName>Levi Santos</UserName>
+      </Profile>
     </Container>
   );
 };
